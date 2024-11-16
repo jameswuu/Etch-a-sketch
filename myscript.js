e7063cadd1cfeefd2322cda7a146772eab19dc85
James Wu <jameswlb01@gmail.com>
2024-11-16 15:19:04 +0800

    Add hovering effect to leave a background color

--- myscript.js
+++ myscript.js
@@ -17,3 +17,15 @@ for (let i = 0; i < 16; i++) {
         console.log(`i is ${i} and j equals to ${j}`);
     }
 }
+
+
+// Add event listener when mouse is on the grid
+const squares = document.querySelectorAll(".square")
+
+// Add the hovering effect
+squares.forEach (square => {
+    // Add background to the div
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "antiquewhite";
+    })
+})
